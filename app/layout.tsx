8e9677efcf409f0a41f73a2047fb8e8b805bcb45
baohuayang16896-jpg <baohuayang16896@gmail.com>
2026-04-03 21:17:59 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
-import { SessionProvider } from "next-auth/react"
+import SessionWrapper from "@/components/SessionWrapper"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -18,7 +18,7 @@ children: React.ReactNode
 return (
 <html lang="en">
 <body className={inter.className}>
-<SessionProvider>{children}</SessionProvider>
+<SessionWrapper>{children}</SessionWrapper>
 </body>
 </html>
 )
